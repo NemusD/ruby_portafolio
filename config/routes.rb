3bf60f825f40a8d01e0e08123fdf207152bea6dd
NemusD <ch.droguett.l@gmail.com>
2023-08-20 22:50:37 -0400

    Se generó la ruta project y se agrego un formulario de contacto

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
-  get 'contact/contact'
-  get 'projects/projects'
-  get 'home/home'
+  #get 'contact/contact'
+  #get 'projects/projects'
+  #get 'home/home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
 
   root 'home#home'
+  get '/contact', to: 'contact#contact'
+  get '/projects', to: 'projects#projects'
+  get '/home', to: 'home#home'
 end
